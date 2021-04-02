ccf0e1be4dab18927adf3aa2cbdb2eabc597726d
Hifume <imagiantporkchop@gmail.com>
2021-04-01 23:50:49 -0600

    Routing, images, about, help

--- src/App.js
+++ src/App.js
@@ -1,8 +1,77 @@
 import './App.css';
+import bigchungus from './assets/bigchungus.png';
+import bigchungusnaturalhabitat from './assets/bigchungusnaturalhabitat.png';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavLink = styled(Link)`
+    color: blue;
+    text-decoration: none;
+    margin-right: 30px;
+`;
 
 const App =_=> {
   return (
-    <h1>Welcome to BigChungus420.com</h1>
+    <Router>
+      <div style={{display: "flex", flex: "1", flexDirection: "column", alignItems: "center", width: "100vw", height: "100vh"}}>
+        <div style={{marginBottom: "30px"}}>
+          <h1 style={{marginBottom: 0}}>Welcome to BigChungus420.com</h1>
+          <h3 style={{marginTop: 0, color: "grey"}}>Your number one source for Big Chungus.</h3>
+          <nav>
+            <NavLink to="/images">Images</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/help">Help</NavLink>
+          </nav>
+        </div>
+
+        <Switch>
+          <Route exact path="/"><Redirect to="/images" /></Route>
+          <Route path="/images">
+            <Images />
+          </Route>
+          <Route path="/about"> 
+            <About />
+          </Route>
+          <Route path="/help"> 
+            <Help />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+const Images =_=> {
+  return (
+    <div style={{display: "flex", flex: "1", flexDirection: "column", alignItems: "center"}}>
+      <button onClick={() => {if(window.confirm("Are you sure? You are leaving BigChungus420.com to go to google.com. BigChungus420.com cannot guarantee that this link is safe.")) { window.open("https://www.google.com/search?q=big+chungus&tbm=isch", "_blank").focus(); }}}>More Chungus</button>
+      <div style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+          <img style={{display: "flex", flex: "0", width: "30vw"}} src={bigchungus} />
+          <p>Big Chungus in a resting state</p>
+        </div>
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+          <img style={{display: "flex", flex: "0", width: "30vw"}} src={bigchungusnaturalhabitat} />
+          <p>Big Chungus in his natural habitat</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const About =_=> {
+  return(
+    <div style={{display: "flex", flex: "1", flexDirection: "column", alignItems: "center"}}>
+      BigChungus420.com was created for the sole purpose of sharing the glorious creature that is Big Chungus with the world.
+    </div>
+  );
+}
+
+const Help =_=> {
+  return(
+    <div style={{display: "flex", flex: "1", flexDirection: "column", alignItems: "center"}}>
+      If you can't see the images, try opening your eyes.
+    </div>
   );
 }
 
